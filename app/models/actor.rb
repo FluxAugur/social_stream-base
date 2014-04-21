077b8b361b3be27562281d12b36c43c02b6cd257
Nathanial L. McConnell <FluxAugur@gmail.com>
2014-04-21 06:25:33 -0400

    updating has_many through uniq

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -13,43 +13,43 @@
 class Actor < ActiveRecord::Base
   @subtypes_name = :subject
   include SocialStream::Models::Supertype
-  
+
   delegate :tag_list, :tag_list=, :tagged_with, :tag_counts, :to => :activity_object
-  
+
   validates_presence_of :name, :subject_type
-  
+
   acts_as_messageable
-  
+
   acts_as_url :name, :url_attribute => :slug
-  
+
   has_one :profile, :dependent => :destroy
 
   has_many :avatars,
            :validate => true,
            :autosave => true
-  		  
+
   has_many :sent_ties,
            :class_name => "Tie",
            :foreign_key => 'sender_id',
            :dependent => :destroy
-  
+
   has_many :received_ties,
            :class_name => "Tie",
            :foreign_key => 'receiver_id',
            :dependent => :destroy
-  
+
   has_many :senders,
-           :through => :received_ties,
-           :uniq => true
-  
+           -> { uniq },
+           :through => :received_ties
+
   has_many :receivers,
-           :through => :sent_ties,
-           :uniq => true
+           -> { uniq },
+           :through => :sent_ties
 
   has_many :spheres
 
   has_many :relation_customs,
-           :through => :spheres, 
+           :through => :spheres,
            :source  => :customs
 
   scope :alphabetic, order('actors.name')
@@ -65,7 +65,7 @@ class Actor < ActiveRecord::Base
       where('actors.name LIKE ?', "%#{ param }%")
     end
   }
-  
+
   scope :tagged_with, lambda { |param|
     if param.present?
       joins(:activity_object).merge(ActivityObject.tagged_with(param))
@@ -81,11 +81,11 @@ class Actor < ActiveRecord::Base
   scope :contacted_from, lambda { |a|
     joins(:received_ties).merge(Tie.sent_by(a).intended)
   }
- 
+
   after_create :create_initial_relations
-  
+
   after_create :save_or_create_profile
-  
+
   class << self
     # Get actor's id from an object, if possible
     def normalize_id(a)
@@ -111,7 +111,7 @@ class Actor < ActiveRecord::Base
         begin
           a.actor
         rescue
-          raise "Unable to normalize actor #{ a.inspect }"        
+          raise "Unable to normalize actor #{ a.inspect }"
         end
       end
     end
@@ -122,15 +122,15 @@ class Actor < ActiveRecord::Base
       find_by_slug! $2
     end
   end
-  
-  
-  
+
+
+
   # The subject instance for this actor
   def subject
     subtype_instance ||
     activity_object.try(:object)
   end
-  
+
   # All the ties sent or received by this actor
   def ties
     Tie.sent_or_received_by(self)
@@ -145,17 +145,17 @@ class Actor < ActiveRecord::Base
       joins(:ties).
       merge(Tie.sent_by(self).received_by(self))
   end
-  
+
   # A given relation defined and managed by this actor
   def relation_custom(name)
     relation_customs.find_by_name(name)
   end
 
-  # The {Relation::Public} for this {Actor} 
+  # The {Relation::Public} for this {Actor}
   def relation_public
     Relation::Public.of(self)
   end
-  
+
   # All the {Actor actors} this one has relation with
   #
   # Options:
@@ -167,12 +167,12 @@ class Actor < ActiveRecord::Base
   def contact_actors(options = {})
     subject_types   = Array(options[:type] || self.class.subtypes)
     subject_classes = subject_types.map{ |s| s.to_s.classify }
-    
+
     as = Actor.select("DISTINCT actors.*").
     where('actors.subject_type' => subject_classes).
     includes(subject_types)
-    
-    
+
+
     case options[:direction]
       when :sent
         as = as.contacted_from(self)
@@ -181,43 +181,43 @@ class Actor < ActiveRecord::Base
     else
       raise "contact actors in both directions are not supported yet"
     end
-    
+
     if options[:include_self].blank?
       as = as.where("actors.id != ?", self.id)
     end
-    
+
     if options[:relations].present?
       as = as.merge(Tie.related_by(options[:relations]))
     end
-    
+
     as
   end
-  
+
   # All the {SocialStream::Models::Subject subjects} that send or receive at least one {Tie} to this {Actor}
   #
-  # When passing a block, it will be evaluated for building the actors query, allowing to add 
+  # When passing a block, it will be evaluated for building the actors query, allowing to add
   # options before the mapping to subjects
   #
   # See #contact_actors for options
   def contacts(options = {})
     as = contact_actors(options)
-    
+
     if block_given?
       as = yield(as)
     end
-    
+
     as.map(&:subject)
   end
-  
+
   # This is an scaffold for a recomendations engine
   #
-  
+
   # Make n suggestions
   # TODO: make more
   def suggestions(n)
     n.times.map{ |m| suggestion }
   end
-  
+
   # By now, it returns a tie suggesting a relation from SuggestedRelations
   # to another subject without any current relation
   #
@@ -230,9 +230,9 @@ class Actor < ActiveRecord::Base
       ( options[:type].present? ?
           Array(options[:type]) :
           self.class.subtypes )
-    
+
     candidates_classes = candidates_types.map{ |t| t.to_s.classify.constantize }
-    
+
     # Candidates are all the instance of "type" minus all the subjects
     # that are receiving any tie from this actor
     candidates = candidates_classes.inject([]) do |cs, klass|
@@ -240,14 +240,14 @@ class Actor < ActiveRecord::Base
       cs -= Array(subject) if subject.is_a?(klass)
       cs
     end
-    
+
     candidate = candidates[rand(candidates.size)]
-    
+
     return nil unless candidate.present?
-    
+
     Contact.new self, candidate.actor.id
   end
-  
+
   # Set of ties sent by this actor received by a
   def ties_to(a)
     sent_ties.received_by(a).intended
@@ -260,7 +260,7 @@ class Actor < ActiveRecord::Base
       Array(sent_ties.create!(:receiver => Actor.normalize(a),
                               :relation => relation_public))
   end
-  
+
   def ties_to?(a)
     ties_to(a).present?
   end
@@ -276,7 +276,7 @@ class Actor < ActiveRecord::Base
   def allow?(subject, action, object = nil)
     allow(subject, action, object).any?
   end
-  
+
   # Can this actor be represented by subject. Does she has permissions for it?
   def represented_by?(subject)
     return false if subject.blank?
@@ -370,7 +370,7 @@ class Actor < ActiveRecord::Base
       Contact.new(self, t.receiver_id)
     end
   end
-  
+
   # The set of {Activity activities} in the wall of this {Actor}.
   #
   # There are two types of walls:
@@ -383,7 +383,7 @@ class Actor < ActiveRecord::Base
   # :for:: the subject that is accessing the wall
   # :relation:: show only activities that are attached at this relation level. For example,
   #             the wall for members of the group.
-  #             
+  #
   def wall(type, options = {})
     ts = ties
 
@@ -399,7 +399,7 @@ class Actor < ActiveRecord::Base
 
     Activity.wall type, ts.all, options
   end
-  
+
   def logo
     avatar!.logo
   end
@@ -407,32 +407,32 @@ class Actor < ActiveRecord::Base
   def avatar!
     avatars.active.first || avatars.build
   end
-  
+
   # The 'like' qualifications emmited to this actor
   def likes
     Activity.joins(:activity_verb).where('activity_verbs.name' => "like").
              joins(:activity_objects).where('activity_objects.id' => activity_object_id)
   end
-  
+
   def liked_by(subject) #:nodoc:
     likes.joins(:ties).where('tie_activities.original' => true).merge(Tie.sent_by(subject))
   end
-  
+
   # Does subject like this {Actor}?
   def liked_by?(subject)
     liked_by(subject).present?
   end
-  
+
   # Build a new activity where subject like this
   def new_like(subject)
     a = Activity.new :verb => "like",
                      :_tie => subject.public_tie_to!(self)
-    
-    a.activity_objects << activity_object           
-                    
-    a             
+
+    a.activity_objects << activity_object
+
+    a
   end
-  
+
   #Returning whether an email should be sent for this object (Message or Notification).
   #Required by Mailboxer gem.
   def should_email?(object)
@@ -443,9 +443,9 @@ class Actor < ActiveRecord::Base
   def to_param
     slug
   end
-  
+
   private
-  
+
   # After create callback
   def create_initial_relations
     Relation::Custom.defaults_for(self)
