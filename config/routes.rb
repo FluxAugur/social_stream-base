8e1693cec4f58f6e2d3f1fcf856ccadcae64650d
Nathanial L. McConnell <FluxAugur@gmail.com>
2014-04-21 08:53:38 -0400

    changing match to get in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,23 @@
 Rails.application.routes.draw do
   #Background tasks
   mount Resque::Server, :at => "/resque"
-  
+
   root :to => "frontpage#index"
-  
-  match 'home' => 'home#index', :as => :home
-  match 'home' => 'home#index', :as => :user_root # devise after_sign_in_path_for
-  
+
+  get 'home' => 'home#index', :as => :home
+  get 'home' => 'home#index', :as => :user_root # devise after_sign_in_path_for
+
   ##API###
-  match 'api/keygen' => 'api#create_key', :as => :api_keygen
-  match 'api/user/:id' => 'api#users'
-  match 'api/me' => 'api#users'
-  match 'api/me/home/' => 'api#activity_atom_feed', :format => 'atom', :as => :api_my_home
-  match 'api/user/:id/public' => 'api#activity_atom_feed', :format => 'atom'
+  get 'api/keygen' => 'api#create_key', :as => :api_keygen
+  get 'api/user/:id' => 'api#users'
+  get 'api/me' => 'api#users'
+  get 'api/me/home/' => 'api#activity_atom_feed', :format => 'atom', :as => :api_my_home
+  get 'api/user/:id/public' => 'api#activity_atom_feed', :format => 'atom'
   ##/API##
-  
+
   # Webfinger
-  match '.well-known/host-meta',:to => 'frontpage#host_meta'
-  
+  get '.well-known/host-meta',:to => 'frontpage#host_meta'
+
   # Social Stream subjects configured in config/initializers/social_stream.rb
   SocialStream.subjects.each do |actor|
     resources actor.to_s.pluralize do
@@ -39,13 +39,13 @@ Rails.application.routes.draw do
   end
   resources :permissions
 
-  match 'tags'     => 'tags#index', :as => 'tags'
-  
+  get 'tags'     => 'tags#index', :as => 'tags'
+
   # Find subjects by slug
-  match 'subjects/lrdd/:id' => 'subjects#lrdd', :as => 'subject_lrdd'
-  
+  get 'subjects/lrdd/:id' => 'subjects#lrdd', :as => 'subject_lrdd'
+
   resource :representation
-  
+
   resources :settings
 
   resources :messages
@@ -53,7 +53,7 @@ Rails.application.routes.draw do
   resources :conversations
 
   resources :invitations
-  
+
   resources :notifications do
     collection do
       put 'update_all'
@@ -65,8 +65,8 @@ Rails.application.routes.draw do
   resources :activities do
     resource :like
   end
-  
-  
+
+
   # Social Stream objects configured in config/initializers/social_stream.rb
   (SocialStream.objects - [ :actor ]).each do |object|
     resources object.to_s.pluralize
